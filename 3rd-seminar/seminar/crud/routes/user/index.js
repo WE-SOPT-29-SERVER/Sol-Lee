be0bf3d91eea3ed8a7366c06f88fee9845616784
sol lee <76844556+soleu@users.noreply.github.com>
2021-10-27 01:39:57 +0900

    add : 프로필 상세보기

--- 3rd-seminar/seminar/crud/routes/user/index.js
+++ 3rd-seminar/seminar/crud/routes/user/index.js
@@ -4,5 +4,6 @@ var router = express.Router();
 
 router.post("/signup", require("./userSignupPOST"));
 router.post("/login", require("./userLoginPOST"));
+router.get("/profile/:id", require("./profileGET"));
 
 module.exports = router;
---
+++ 3rd-seminar/seminar/crud/routes/user/profileGET.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const responseMessage = require("../../constants/responseMessage");
+const statusCode = require("../../constants/statusCode");
+const users = require("../../dbMockup/user");
+const util = require("../../lib/util");
+const router = express.Router();
+
+/*
+get profile
+METHOD : GET
+URI : localhost:3000/user/profile/:id
+RESPONSE STATUS :200(OK)
+RESPONSE DATA :비밀번호를 제외한  User정보
+*/
+
+//  /user/login
+router.get("/profile/:id", async (req, res) => {
+    //req params에서 데이터 가져오기
+    const id = parseInt(req.params.id);
+    //존재하는 아이디인지 확인 - 없다면 No user 반환
+    const user = users.filter(user => user.id === id)[0];
+
+    if (!user) {
+        return res.status(statusCode.BAD_REQUEST).send(util.fail(statusCode.BAD_REQUEST, responseMessage.NO_USER));
+    }
+
+    //성공 - login success와 함께 userId 반환
+    res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.LOGIN_SUCCESS, {
+        "userId": id
+    }));
+
+})
+
+module.exports = router;

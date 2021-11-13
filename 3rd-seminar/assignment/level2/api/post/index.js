5f289cc17ed8f78b4fdeb8d73e061dd43ef33ebb
sol lee <76844556+soleu@users.noreply.github.com>
2021-11-13 11:06:24 +0900

    feat: update post

--- 3rd-seminar/assignment/level2/api/post/index.js
+++ 3rd-seminar/assignment/level2/api/post/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 router.get("/", require("./postAllGET"));
 router.get("/:id", require("./postGET"));
 router.post("/", require("./postPOST"));
-// router.put("/:id", require("./postPUT"));
+router.put("/:id", require("./postPUT"));
 // router.delete("/:id", require("./postDELETE"));
 
 module.exports = router;
---
+++ 3rd-seminar/assignment/level2/api/post/postPUT.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const responseMessage = require("../../constants/responseMessage");
+const statusCode = require("../../constants/statusCode");
+const posts = require("../../dbMockup/post");
+const util = require("../../lib/util");
+const router = express.Router();
+
+/*
+update post
+METHOD : PUT
+URI : localhost:3000/post/:id
+REQUEST DATA :newPost
+RESPONSE STATUS :200(OK)
+RESPONSE DATA: 수정된 글 정보
+*/
+
+//  /post/:id
+router.put("/:id", async (req, res) => {
+    const {
+        id
+    } = req.params;
+    const {
+        newTitle,
+        newContents
+    } = req.body;
+
+    if (!id) {
+        return res.status(statusCode.BAD_REQUEST)
+            .send(
+                util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE),
+            );
+    }
+
+    const existingPost = posts.filter(post => post.id === Number(id))[0];
+    if (!existingPost) {
+        return res
+            .status(statusCode.BAD_REQUEST)
+            .send(util.fail(statusCode.BAD_REQUEST, responseMessage.NO_USER));
+    }
+
+    const updatePost = {
+        ...existingPost,
+        title: newTitle,
+        contents: newContents
+    };
+    res.status(statusCode.OK).send(
+        util.success(statusCode.OK, responseMessage.USER_UPDATE_SUCCESS, updatePost, ),
+    )
+})
+
+module.exports = router;

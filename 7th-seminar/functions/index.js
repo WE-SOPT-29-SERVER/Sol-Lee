e6b235ae14173d00134f737448e8a24ccf723283
soleu <dlthf555@gmail.com>
2021-12-23 23:20:36 +0900

    add: 7th-seminar firebase setting

---
+++ 7th-seminar/functions/index.js
@@ -0,0 +1,9 @@
+const functions = require('firebase-functions');
+const serviceAccount = require('./wesopt29-b36aa-firebase-adminsdk-5slhh-bae2660f8b');
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });

8241e41e871aaad14a34d6c02898c43b8357e71a
soleu <dlthf555@gmail.com>
2021-12-24 22:46:04 +0900

    add: image upload

--- 7th-seminar/functions/api/routes/post/index.js
+++ 7th-seminar/functions/api/routes/post/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const { checkUser } = require('../../../middlewares/auth');
 const router = express.Router();
 
+const uploadImage = require('../../../middlewares/uploadImage');
 router.get('/list', require('./postListGET'));
-router.post('/', require('./postPOST'));
+router.post('/', uploadImage, require('./postPOST'));
 
-router.get('/:postId', require('./postGET'));
+router.get('/:postId', checkUser, require('./postGET'));
 router.put('/:postId', require('./postPUT'));
 router.delete('/:postId', require('./postDELETE'));
 

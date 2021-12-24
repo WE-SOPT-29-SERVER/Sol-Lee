f06c4238d57ce6465960c3115008c42e1b0805eb
soleu <dlthf555@gmail.com>
2021-12-25 00:57:27 +0900

    add: refresh token 구현 완료

--- 7th-seminar/functions/api/routes/post/postGET.js
+++ 7th-seminar/functions/api/routes/post/postGET.js
@@ -15,11 +15,11 @@ module.exports = async (req, res) => {
     client = await db.connect(req);
 
     const post = await postDB.getPostById(client, postId);
-    res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.READ_ONE_POST_SUCCESS, post));
+    return res.status(statusCode.OK).send(util.success(statusCode.OK, responseMessage.READ_ONE_POST_SUCCESS, post));
   } catch (error) {
     functions.logger.error(`[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`, `[CONTENT] ${error}`);
     console.log(error);
-    res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
+    return res.status(statusCode.INTERNAL_SERVER_ERROR).send(util.fail(statusCode.INTERNAL_SERVER_ERROR, responseMessage.INTERNAL_SERVER_ERROR));
   } finally {
     client.release();
   }
